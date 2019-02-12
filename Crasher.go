b1b30ade22a66c18abb844223a0a917e6d5fe2e9
Simon Tucker <simon@brotherlogic@gmail>
2019-02-12 15:56:31 -0800

    Fix

--- Crasher.go
+++ Crasher.go
@@ -28,6 +28,11 @@ func (s *Server) ReportHealth() bool {
 	return true
 }
 
+// Shutdown shutsdown the server
+func (s *Server) Shutdown(ctx context.Context) error {
+	return nil
+}
+
 // Mote promotes this server
 func (s *Server) Mote(ctx context.Context, master bool) error {
 	//No mote
